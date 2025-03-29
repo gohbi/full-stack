316feb20ddf8718cf353c0fcfecc48986f1bba6a
Reanna Francis <187709664+gohbi@users.noreply.github.com>
2025-03-29 03:14:53 +0000

    Lots of full stack dev project code

---
+++ back-end/src/server.js
@@ -0,0 +1,50 @@
+import express, { request } from 'express';
+
+const articleInfo = [
+    {name: 'learn-flask', upvotes: 0, comments: []},
+    {name: 'learn-node', upvotes: 0, comments: []},
+    {name: 'learn-react', upvotes: 0, comments: []},
+]
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/api/articles/:name/upvote', (req, res) =>{
+    const article = articleInfo.find(a => a.name == req.params.name);
+    article.upvotes += 1;
+
+    res.json(article);
+});
+
+app.post('/api/articles/:name/comments', (req, res) =>{
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articleInfo.find(a => a.name == name);
+
+    article.comments.push({
+        postedBy,
+        text,
+    });
+
+    res.json(article);
+});
+
+
+/*
+app.get('/hello', function(req, res){
+    res.send('Hello from a GET endpoint!');
+});
+
+app.get('/hello/:name', function(req,res){
+    res.send('Hello,' + req.params.name );
+});
+
+app.post('/hello', function(req, res){
+    res.send('Hello' +  req.body.name + 'from a POST endpoint!');
+});
+*/
+app.listen(8000, function(){
+    console.log('Server is listening on port 8000');
+});
---
+++ front-end/src/Layout.jsx
@@ -0,0 +1,12 @@
+import NavBar from './NavBar';
+import { Outlet } from 'react-router-dom';
+
+export default function Layout() {
+
+  return (
+    <>
+    <NavBar />
+    <Outlet/>
+    </>
+  );
+}
