5c8c99f3623c3f5d2dddda223f7ff0cbd764595e
Reanna Francis <187709664+gohbi@users.noreply.github.com>
2025-07-02 04:37:28 +0000

    NavBar styling fixes

--- front-end/src/NavBar.jsx
+++ front-end/src/NavBar.jsx
@@ -3,16 +3,28 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
     return(
         <nav className="site-nav text-uppercase navbar navbar-expand-mb navbar-dark w-100 bg-primary">
-
             <div className="container-fluid">
                 <a className="navbar-brand font-weight-normal" href="/"><i className="fas fa-cube mr-2"></i> Info Grazers</a>
-
-                <div className="navbar-nav ml-auto">
-                    <a className="nav-item nav-link" href="/"><i className="fas fa-home mr-2"></i> Home</a>
-                    <a className="nav-item nav-link" href="/about"><i className="fas fa-info-circle"></i> About</a>
-                    <a className="nav-item nav-link" href="/articles"><i className="fas fa-newspaper"></i> Articles</a>
-                </div>
+             
+            
+                <button 
+                type="button" 
+                className="navbar-toggler" 
+                data-toggle="collapse" 
+                data-target="#myTogglerNav" 
+                aria-controls="#myTogglerNav"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <section className="collapse navbar-collapse" id="myTogglerNav">
+                    <div className="navbar-nav ml-auto">
+                        <a className="nav-item nav-link" href="/"><i className="fas fa-home"></i> Home</a>
+                        <a className="nav-item nav-link" href="/about"><i className="fas fa-info-circle"></i> About</a>
+                        <a className="nav-item nav-link" href="/articles"><i className="fas fa-newspaper"></i> Articles</a>
+                    </div>
+                </section>
             </div>
         </nav>
+
     )
 }
