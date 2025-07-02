88bff28ad52b98e57d2319107d87f97bf6f526f7
Reanna Francis <187709664+gohbi@users.noreply.github.com>
2025-07-02 03:47:31 +0000

    UX fixes

--- front-end/src/NavBar.jsx
+++ front-end/src/NavBar.jsx
@@ -2,18 +2,17 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
     return(
-        <nav>
-            <ul>
-                <li>
-                    <Link to='/'> Home</Link>
-                </li>
-                <li>
-                    <Link to='/about'>About</Link> 
-                </li>
-                <li>
-                    <Link to='/articles'>Articles</Link>
-                </li>
-            </ul>
+        <nav className="site-nav text-uppercase navbar navbar-expand-mb navbar-dark w-100 bg-primary">
+
+            <div className="container-fluid">
+                <a className="navbar-brand font-weight-normal" href="/"><i className="fas fa-cube mr-2"></i> Info Grazers</a>
+
+                <div className="navbar-nav ml-auto">
+                    <a className="nav-item nav-link" href="/"><i className="fas fa-home mr-2"></i> Home</a>
+                    <a className="nav-item nav-link" href="/about"><i className="fas fa-info-circle"></i> About</a>
+                    <a className="nav-item nav-link" href="/articles"><i className="fas fa-newspaper"></i> Articles</a>
+                </div>
+            </div>
         </nav>
     )
 }
